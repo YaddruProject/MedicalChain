f572cb18565717870abcaca9fdb1a1de34ea4b18
PrajjuS <theprajjus@gmail.com>
2024-10-06 14:03:31 +0530

    MedicalChain/client: Implement HomePage
    Change-Id: I2f644830aceec9262d01c69d275db1a925c42a24
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,6 +1,87 @@
+import { Layout, Button, Typography, Row, Col, Divider, Card, Grid } from 'antd';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const { Title, Paragraph } = Typography;
+const { Content } = Layout;
+const { useBreakpoint } = Grid;
+
 const HomePage = () => {
+  const screens = useBreakpoint();
+
   return (
-    <div>HomePage</div>
+    <Layout>
+      <Header />
+      <Content className='hero'>
+        <div className='hero-content'>
+          <Title level={screens.xs ? 2 : 1}>
+            Welcome to Medical Chain System
+          </Title>
+          <Paragraph>
+            Our Electronic Health Records (EHR) system uses blockchain technology to ensure the security and privacy of your medical data.
+          </Paragraph>
+          <Button type='primary' size='large'>
+            Learn More
+          </Button>
+        </div>
+      </Content>
+      <Content style={{ padding: '0 24px 24px '}}>
+        <Row gutter={[16, 24]} justify={'center'} align={'center'}>
+          <Col xs={24} sm={12} md={8}>
+            <Card className='content-card' bordered={false}>
+              <Title level={screens.xs ? 3 : 2}>Secure</Title>
+              <Paragraph>
+                Our system ensures that all medical records are encrypted and stored securely on the blockchain. Unauthorized access is prevented through robust security protocols.
+              </Paragraph>
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8}>
+            <Card className='content-card' bordered={false}>
+              <Title level={screens.xs ? 3 : 2}>Transparent</Title>
+              <Paragraph>
+                With blockchain technology, every transaction is recorded and can be traced for added transparency. Patients and healthcare providers can verify records easily and accurately.
+              </Paragraph>
+            </Card>
+          </Col>
+          <Col xs={24} sm={12} md={8}>
+            <Card className='content-card' bordered={false}>
+              <Title level={screens.xs ? 3 : 2}>Efficient</Title>
+              <Paragraph>
+                Streamline data access and sharing with an efficient and decentralized system. Reduce paperwork and administrative overhead with our automated processes.
+              </Paragraph>
+            </Card>
+          </Col>
+        </Row>
+        <Divider />
+        <Row gutter={[16, 24]} justify={'center'} align={'center'}>
+          <Col xs={24} lg={12}>
+            <Card className='content-card' bordered={false} style={{ height: '100%' }}>
+              <Title level={screens.xs ? 3 : 2}>Features</Title>
+              <Paragraph>
+                Decentralized Storage: Ensure data integrity and reliability with decentralized storage.
+              </Paragraph>
+              <Paragraph>
+                Smart Contracts: Automate and enforce permissions through smart contracts.
+              </Paragraph>
+              <Paragraph>
+                Interoperability: Seamlessly integrate with existing healthcare systems and standards.
+              </Paragraph>
+            </Card>
+          </Col>
+          <Col xs={24} lg={12}>
+            <Card className='content-card' bordered={false} style={{ height: '100%' }}>
+              <Title level={screens.xs ? 3 : 2}>Get Started</Title>
+              <Paragraph>
+                Ready to experience the future of healthcare? Get started with our EHR system today. Contact us to schedule a demo or request more information.
+              </Paragraph>
+              <Button type='primary'>Contact Us</Button>
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+      <Footer />
+    </Layout>
   )
 }
 
