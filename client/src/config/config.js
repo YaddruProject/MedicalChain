1785295b911868a2e130ba3884837bdd42f8e817
PrajjuS <theprajjus@gmail.com>
2024-10-20 10:13:11 +0530

    MedicalChain/client: Change contract ABI parsing logic in config
    - Change from dynamic import to direct assignment for contract ABI in
      config.
    - Also update env structure.
    
    Change-Id: If4f34fb458089d54d1edd2501da222f10f5eaf50
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,7 +2,20 @@ import { z } from 'zod';
 
 const configSchema = z.object({
     VITE_CONTRACT_ADDRESS: z.string(),
-    VITE_CONTRACT_ABI: z.string(),
+    VITE_CONTRACT_ABI: z.string().transform((val) => {
+        try {
+            const parsedJson = JSON.parse(val);
+            return parsedJson;
+            // TODO: Validate ABI - IDK why tf it's not working
+            // const abi = z.array(z.object()).safeParse(parsedJson);
+            // if (!abi.success) {
+            //     throw new Error('Invalid ABI: ' + abi.error.errors);
+            // }
+            // return abi.data;
+        } catch (error) {
+            throw new Error('Invalid ABI: ' +  error);
+        }
+    }),
     VITE_PINATA_JWT: z.string(),
     VITE_PINATA_GATEWAY: z.string(),
 });
@@ -14,16 +27,9 @@ class Config {
             throw new Error('Please fill configs in .env file');
         }
         this.CONTRACT_ADDRESS = config.data.VITE_CONTRACT_ADDRESS;
-        this.loadABI(config.data.VITE_CONTRACT_ABI).then((abi) => {
-            this.CONTRACT_ABI = abi.abi;
-        });
+        this.CONTRACT_ABI = config.data.VITE_CONTRACT_ABI;
         this.PINATA_JWT = config.data.VITE_PINATA_JWT;
         this.PINATA_GATEWAY = config.data.VITE_PINATA_GATEWAY;
-    }
-    
-    async loadABI(path) {
-        const abi = await import(`../../../web3/artifacts/contracts/${path}.sol/${path}.json`);
-        return abi;
     }
 }
 
