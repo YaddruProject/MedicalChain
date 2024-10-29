026c6829393104cd7787a0a3c26204d7ffda0bcd
PrajjuS <theprajjus@gmail.com>
2024-10-29 21:24:19 +0530

    MedicalChain/web3: Update ignition module according to the latest contract changes
    Change-Id: Ib42dd076196a32ef10c13d39216160224dc4a667
    Signed-off-by: PrajjuS <theprajjus@gmail.com>

--- web3/ignition/modules/MedicalChain.js
+++ web3/ignition/modules/MedicalChain.js
@@ -3,13 +3,17 @@
 
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const ADDRESS = "0x425a9700d783bAC4CD3c9EE8578b9FF8a14EBF96";
 const NAME = "ADMIN";
-const CONTACT = "123456";
+const EMAIL = "example@okmail.com";
+const CONTACT = "1234567890";
 
 module.exports = buildModule("MedicalChainModule", (m) => {
 
-  const medicalchain = m.contract("MedicalChain", [ADDRESS, NAME, CONTACT]);
+  const medicalchain = m.contract(
+    "MedicalChain",
+    [ADDRESS, NAME, EMAIL, CONTACT]
+  );
 
   return { medicalchain };
 });
